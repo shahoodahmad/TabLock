c6eec77308ec0f7cd0c281b67a31a6a1294d5ef5
ddurdall <57512105+ddurdall@users.noreply.github.com>
2021-01-14 15:14:07 -0500

    Adding pinpad to extension

--- pinpad.js
+++ pinpad.js
@@ -1,10 +1,18 @@
 let input = document.getElementById("pad");
 
+
+for (let i = 0; i < 11; i++){
+  document.getElementById(`${i}`).addEventListener("click", function() {
+    clicked(`${i}`)
+  });
+}
+
+
 function clicked(string){
   let len = input.value.length;
-  if (string != "back" && len<6){
+  if (string != "10" && len<6){
     input.value += string;
-  } else if(string == "back"){
+  } else if(string == "10"){
     input.value = input.value.substring(0, len-1);
   }
 }
