694ae1192b184cde2c48542d039a13c0f0de153d
Shahood Ahmad <54773167+shahoodahmad@users.noreply.github.com>
2021-01-19 15:05:03 -0500

    Error checking and design features

--- instance.js
+++ instance.js
@@ -45,11 +45,11 @@ function setupInstance(tabInstance){
     let delBtn = document.createElement("span");
     btn.appendChild(document.createTextNode(tabInstance.name));
     btn.setAttribute("class", "instance");
-    btn.setAttribute("id", count); //will replace count with the name of the
+    btn.setAttribute("id", tabInstance.name); //will replace count with the name of the
     delBtn.appendChild(document.createTextNode("X"));
-    let br = document.createElement("br");
+    //let br = document.createElement("span");
     delBtn.setAttribute("class", "close");
-    br.setAttribute("id", "br" + count);
+    //br.setAttribute("id", "br" + count);
     btn.addEventListener("click", function(){
       if(!delClicked){
         chrome.tabs.create({url: tabInstance.URLs, active: false});
@@ -59,21 +59,32 @@ function setupInstance(tabInstance){
       }
     });
     delBtn.addEventListener("click", function(){
-      div.removeChild(document.getElementById(count));
-      div.removeChild(document.getElementById("br" + count));
+      div.removeChild(document.getElementById(tabInstance.name));
+      //div.removeChild(document.getElementById("br" + count));
       count--;
       delClicked = true;
     });
 
     div.appendChild(btn);
     btn.appendChild(delBtn);
-    div.appendChild(br);
+    //div.appendChild(br);
 }
 
 document.getElementById("tab").addEventListener("click", function(){
     chrome.tabs.query({active: true}, function(tabs){
 
         let insName = prompt("Enter a name");
+        while (insName == "" || insName.length > 10 || inGroup(insName)){
+          if(insName == ""){
+            insName = prompt("Invalid entry: Enter another name");
+          } else if (insName.length > 10){
+            insName = prompt("Name cannot exceed 10 characters: Enter another name");
+          }
+          else {
+            insName = prompt("Name already in use, enter a new name");
+          }
+        }
+
         if(insName != null){
           let tabInstance = new instance(insName, tabs[0].url, tabs[0].id);
           setupInstance(tabInstance);
@@ -81,5 +92,28 @@ document.getElementById("tab").addEventListener("click", function(){
         }
 
     });
+
+});
+
+function inGroup(){
+  return false;
+}
+
+let store = document.getElementById("storage");
+store.addEventListener("click", function(){
+
+  let ins = new instance("bruh", "http://www.google.com", "abcde");
+  let ins2 = new instance("yeet", "http://www.github.com", "defgh");
+  let insArr = [ins, ins2];
+
+  chrome.storage.sync.set({key: insArr}, function(){
+    alert("saved");
+  });
+
+  chrome.storage.sync.get("key", function(obj){
+    console.log(obj);
+  });
+
+  chrome.storage.sync.clear();
 
 });
