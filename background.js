e6e6294643bb4a46c21e3bd7f96372bc85049da2
ddurdall <57512105+ddurdall@users.noreply.github.com>
2021-01-21 15:03:52 -0500

    storage api integration

--- background.js
+++ background.js
@@ -2,4 +2,7 @@
 //Installation setup
 chrome.runtime.onInstalled.addListener(function() {
     
+    //on installation (or reload), setup empty tab instance array and store
+    let instances = [];
+    chrome.storage.sync.set({key: instances});
 });
--- instance.js
+++ instance.js
@@ -1,3 +1,21 @@
+let instances = [];
+
+//retrieve instances array from storage and add buttons for each instance
+chrome.storage.sync.get("key", function(obj){
+  instances = obj.key;
+
+  if (instances.length > 0){
+    
+    //putting instance buttons on screen
+    for (let i = 0; i < instances.length; i++){
+      setupInstance(instances[i]);
+    }
+  }
+
+});
+
+
+
 
 //tab instance object constructor
 function instance(instance_name, url_list, id_list){
@@ -23,33 +41,24 @@ function decryptUrl(){
 
 }
 
-function unpackInstance(){
-
-}
-
-
-/*let test = document.getElementById("test");
-test.addEventListener("click", function(){
-    let body = document.getElementById("body");
-    body.removeChild(document.getElementById(count));
-    body.removeChild(document.getElementById("br" + count));
-    count--;
-});*/
-
-let count = 0;
 
 function setupInstance(tabInstance){
-    let delClicked = false;
+    let delClicked = false; //flag var
+
     let div = document.getElementById("div");
     let btn = document.createElement("button");
     let delBtn = document.createElement("span");
+    
+    //setup instance button
     btn.appendChild(document.createTextNode(tabInstance.name));
     btn.setAttribute("class", "instance");
     btn.setAttribute("id", tabInstance.name); //will replace count with the name of the
+    
+    //setup delete button
     delBtn.appendChild(document.createTextNode("X"));
-    //let br = document.createElement("span");
     delBtn.setAttribute("class", "close");
-    //br.setAttribute("id", "br" + count);
+    
+    //loads tabs from instance
     btn.addEventListener("click", function(){
       if(!delClicked){
         chrome.tabs.create({url: tabInstance.URLs, active: false});
@@ -58,23 +67,30 @@ function setupInstance(tabInstance){
         delClicked = false;
       }
     });
+
+    //deletes instance button and removes from array
     delBtn.addEventListener("click", function(){
       div.removeChild(document.getElementById(tabInstance.name));
-      //div.removeChild(document.getElementById("br" + count));
-      count--;
+      let pos = binSearch(instances, tabInstance);
+      
+      //remove from array and update storage
+      instances.splice(pos, 1);
+      chrome.storage.sync.set({key: instances});
       delClicked = true;
     });
 
     div.appendChild(btn);
     btn.appendChild(delBtn);
-    //div.appendChild(br);
 }
 
 document.getElementById("tab").addEventListener("click", function(){
     chrome.tabs.query({active: true}, function(tabs){
 
         let insName = prompt("Enter a name");
-        while (insName == "" || insName.length > 10 || inGroup(insName)){
+        let tabInstance = new instance(insName, tabs[0].url, tabs[0].id);
+        
+        //error checking for invalid names
+        while (insName == "" || insName.length > 10 || inGroup(instances, tabInstance)){
           if(insName == ""){
             insName = prompt("Invalid entry: Enter another name");
           } else if (insName.length > 10){
@@ -83,37 +99,72 @@ document.getElementById("tab").addEventListener("click", function(){
           else {
             insName = prompt("Name already in use, enter a new name");
           }
+          tabInstance = new instance(insName, tabs[0].url, tabs[0].id);
         }
 
         if(insName != null){
-          let tabInstance = new instance(insName, tabs[0].url, tabs[0].id);
+          
           setupInstance(tabInstance);
+          
+          instances.push(tabInstance);
+
+          //sort instances alphabetically by name
+          instances.sort(function(x, y){
+            if (x.name > y.name){
+              return 1;
+            } else {
+              return -1;
+            }
+          });
+          chrome.storage.sync.set({key: instances});
+          
+          //TODO: remove tab after instance is created  
           //chrome.tabs.remove(tabInstance.IDs);
+        
         }
 
     });
 
 });
 
-function inGroup(){
-  return false;
+//checks if instance is already in instance array
+function inGroup(instances, elem){
+  if (binSearch(instances, elem) == -1){
+    return false;
+  } else {
+    return true;
+  }
 }
 
-let store = document.getElementById("storage");
-store.addEventListener("click", function(){
+function compare(x, y){
+  if (x.name > y.name){
+    return 1;
+  } else if (x.name == y.name){
+    return 0;
+  } else {
+    return -1;
+  }
+}
 
-  let ins = new instance("bruh", "http://www.google.com", "abcde");
-  let ins2 = new instance("yeet", "http://www.github.com", "defgh");
-  let insArr = [ins, ins2];
+function binSearch(instances, elem){
+  let start = 0;
+  let end = instances.length;
 
-  chrome.storage.sync.set({key: insArr}, function(){
-    alert("saved");
-  });
+  while (start < end){
+    let mid = Math.floor(start + (end - start)/2);
+    if (compare(instances[mid], elem) == 0){//found
+      return mid;
+    }else if (compare(instances[mid], elem) == 1){ //mid > elem
+      end = mid;
+    } else {//mid < elem
+      start = mid + 1;
+    } 
 
-  chrome.storage.sync.get("key", function(obj){
-    console.log(obj);
-  });
+  }
 
-  chrome.storage.sync.clear();
+  return -1;
 
-});
+}
+
+
+
