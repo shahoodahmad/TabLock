f2d172e232f5ce42f23fbcc81151c9de0321864a
David Durdaller <ddurdall@gmail.com>
2022-01-11 14:06:10 -0500

    Redesigned ui, commenting code, bug fixing

--- instance.js
+++ instance.js
@@ -1,11 +1,13 @@
+//TODO: better description of how to use the extension (tooltips, maybe usage guide at bottom)
+//TODO: add some css animations maybe
+//TODO: README on github repo
+//      installation guide
+//      usage instructions
+//      explanation of features
 
-//TODO: more intuitive ui
-//      Color code on windows vs tab instances
-//      better layout
-//      better description of how to use the extension
-//TODO: Tidy up code with comments
-
+//Global variable for storing all the instances
 let instances = [];
+let reservedKeywords = ['header', 'tablock', 'hdr-text', 'container', 'mainDiv', 'tabBtn', 'windowBtn', 'tabContainer', 'windowContainer', 'divider']
 
 //retrieve instances array from storage and add buttons for each instance
 chrome.storage.sync.get("key", function(obj){
@@ -27,21 +29,32 @@ function instance(instance_name, url_list, pwd, slt, isWindow){
     this.name = instance_name; //Name of instance to be displayed
     this.URLs = url_list; //list of encrypted URLs
     this.pin = pwd; //password for the instance
-    this.salt = slt;
-    this.isWindow = isWindow; 
+    this.salt = slt; //generated salt
+    this.isWindow = isWindow; //true if the instance is a window
 }
 
+//function that handles populating the instance buttons onto the screen and aditional setup
 function setupInstance(tabInstance){
     let delClicked = false; //flag var
+    let containerType;
 
-    let div = document.getElementById("div");
+    //decides to add the button to the window container or tab container
+    if (tabInstance.isWindow) {
+        containerType = "windowContainer";
+    } else {
+        containerType = "tabContainer";
+    }
+
+    //find objects to be modified
+    let container = document.getElementById(containerType);
     let btn = document.createElement("button");
     let delBtn = document.createElement("span");
 
-    //setup instance button
+    //setup new instance button
     btn.appendChild(document.createTextNode(tabInstance.name));
     btn.setAttribute("class", "instance");
-    btn.setAttribute("id", tabInstance.name); //will replace count with the name of the
+    btn.setAttribute("id", tabInstance.name); //TODO: possible vulnerability here
+                                              //if they name it after one of the preset ids, bad
 
     //setup delete button
     delBtn.appendChild(document.createTextNode("X"));
@@ -49,12 +62,14 @@ function setupInstance(tabInstance){
 
     //loads tabs from instance
     btn.addEventListener("click", function(){
+      //prevents the button from being clicked if the close button is clicked
       if(!delClicked){
         //salt and hash attempted password
         pwd = prompt("Enter your password.");
         salt = tabInstance.salt;
         hashedPwd = CryptoJS.SHA256(pwd + salt).toString();
 
+        //checks if entered password hash matches stored hash
         if (hashedPwd == tabInstance.pin) {
             for (let i = 0; i < tabInstance.URLs.length; i++){
                 let decryptedUrl = CryptoJS.AES.decrypt(tabInstance.URLs[i], pwd).toString(CryptoJS.enc.Utf8);
@@ -72,8 +87,8 @@ function setupInstance(tabInstance){
 
     //deletes instance button and removes from array
     delBtn.addEventListener("click", function(){
-      div.removeChild(document.getElementById(tabInstance.name));
-      let pos = binSearch(instances, tabInstance);
+      container.removeChild(document.getElementById(tabInstance.name));
+      let pos = binSearch(instances, tabInstance.name);
 
       //remove from array and update storage
       instances.splice(pos, 1);
@@ -81,7 +96,7 @@ function setupInstance(tabInstance){
       delClicked = true;
     });
 
-    div.appendChild(btn);
+    container.append(btn);
     btn.appendChild(delBtn);
 }
 
@@ -91,12 +106,11 @@ let createBtns = document.getElementsByClassName("create");
         createBtns[i].addEventListener("click", function(){
             let btnid = this.id;
             //Query focused window and get the active tab in that window
-            chrome.windows.getLastFocused({populate: true}, function(wind){
-                console.log(wind);
+            chrome.windows.getCurrent({populate: true}, function(wind){
                 let tabArray = wind.tabs;
 
                 var isWindow = true;
-                if (btnid == "tab") {
+                if (btnid == "tabBtn") {
                   isWindow = false;
                     for (let i = 0; i < tabArray.length; i++){
                         if (tabArray[i].active == true){
@@ -106,61 +120,89 @@ let createBtns = document.getElementsByClassName("create");
                 }
 
                 let insName = prompt("Enter a name");           
-      
-                //error checking for invalid names
-                while (insName == "" || insName.length > 10 || inGroup(instances, insName)){
-                    if(insName == ""){
-                        insName = prompt("Invalid entry: Enter another name");
-                    } else if (insName.length > 10){
-                        insName = prompt("Name cannot exceed 10 characters: Enter another name");
-                    }
-                    else {
-                        insName = prompt("Name already in use, enter a new name");
-                    }  
+                
+                //only proceed if the user entered a name
+                if (insName == null){
+                    return;
+                }
+
+                //error check name
+                insName = nameCheck(insName);
+                
+                //user may have cancelled during error checking
+                if (insName == null){
+                    return;
                 }
 
                 let pwd = prompt("Enter a password for your instance");
                 
-                //encrypt each url
-                let encUrl = [];
-                for (let i = 0; i < tabArray.length; i++){
-                    encUrl.push(CryptoJS.AES.encrypt(tabArray[i].url, pwd));
-                }
+                let tabInstance = generateInstance(insName, pwd, tabArray, isWindow);
 
-                //generate 256 bit salt and hash + store password
-                let salt = CryptoJS.lib.WordArray.random(32).toString();
-                pwd = CryptoJS.SHA256(pwd.concat(salt)).toString();
-        
-                let tabInstance = new instance(insName, encUrl, pwd, salt, isWindow);
+                setupInstance(tabInstance);
+                instances.push(tabInstance);
 
-                if(insName != null){
-                    setupInstance(tabInstance);
-                    instances.push(tabInstance);
-
-                    //sort instances alphabetically by name
-                    instances.sort(function(x, y){
-                        if (x.name > y.name){
-                            return 1;
-                        } else {
-                            return -1;
-                        }
-                    });
-                    chrome.storage.sync.set({key: instances});
-                }
+                 //sort instances alphabetically by name
+                 instances.sort(function(x, y){
+                     if (x.name > y.name){
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                 });
+                 chrome.storage.sync.set({key: instances});
 
             }); //window query
         }); //event listener
     }//end for loop
 
-//checks if instance is already in instance array
+//crypto and creation of instance
+function generateInstance(insName, pwd, tabArray, isWindow){
+
+        //encrypt each url
+        let encUrl = [];
+        for (let i = 0; i < tabArray.length; i++){
+            encUrl.push(CryptoJS.AES.encrypt(tabArray[i].url, pwd));
+        }
+
+        //generate 256 bit salt and hash + store password
+        let salt = CryptoJS.lib.WordArray.random(32).toString();
+        pwd = CryptoJS.SHA256(pwd.concat(salt)).toString();
+        let tabInstance = new instance(insName, encUrl, pwd, salt, isWindow);
+        return tabInstance;
+}
+
+//error checking for invalid names
+function nameCheck(insName){
+
+    while (insName == "" || insName.length > 10 || inGroup(instances, insName)){
+        if(insName == ""){
+            insName = prompt("Invalid entry: Enter another name");
+        } else if (insName.length > 10){
+            insName = prompt("Name cannot exceed 10 characters: Enter another name");
+        }
+        else {
+            insName = prompt("Name already in use, enter a new name");
+        }  
+        if (insName == null){
+            break;
+        }
+    }
+    return insName;
+}
+
+//checks if instance is already in instance array or if the name is a reserved keyword
 function inGroup(instances, elem){
   if (binSearch(instances, elem) == -1){
+    if (reservedKeywords.includes(elem)){
+        return true;
+    }
     return false;
   } else {
     return true;
   }
 }
 
+//compares instance objects by name
 function compare(x, y){
   if (x.name > y){
     return 1;
@@ -171,6 +213,7 @@ function compare(x, y){
   }
 }
 
+//binary search for searching through instance array
 function binSearch(instances, elem){
   let start = 0;
   let end = instances.length;
@@ -191,11 +234,3 @@ function binSearch(instances, elem){
 
 }
 
-
-//let iFr = document.getElementById("pad-frm");
-//let elem = iFr.contentDocument.getElementById("1");
-//elem.style.display = "none";
-
-//window.frames['pad_frm'].document.getElementById('1').addEventListener("click", function(){
-  //  alert("1 pressed");
-//});
